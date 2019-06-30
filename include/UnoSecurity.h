32b5d5cd545510ba6eda900cc95af2a69cd1d0aa
Serhii Tymoshenko <temosh@i.ua>
2019-06-30 16:00:54 +0300

    Initial project structure

---
+++ include/UnoSecurity.h
@@ -0,0 +1,9 @@
+//
+// Created by svtem on 30.06.2019.
+//
+
+#ifndef UNO_SECURITY_H
+#define UNO_SECURITY_H
+
+
+#endif //UNO_SECURITY_H
---
+++ src/UnoSecurity.cpp
@@ -0,0 +1,13 @@
+//
+// Created by svtem on 30.06.2019.
+//
+
+#include "UnoSecurity.h"
+
+void setup() {
+
+}
+
+void loop() {
+
+}
